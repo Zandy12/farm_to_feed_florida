f9ba5ae5b09a85d438c06e371b336cd6866717de
Zane Chandy <zanechandy@Zanes-MacBook-Pro.local>
2020-10-24 18:22:28 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
+// Import libraries
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+// Import images
+import officeDepot from './images/office-depot.jpg';
+import bocaCode from './images/boca-code.png';
+import aws from './images/aws.jpg';
+import fpl from './images/fpl.jpg';
+import fsfHeader from './images/fsf-header.png';
+import hamburgerIcon from './images/hamburger-icon.png';
+
+// Import CSS
 import './App.css';
 
 function App() {
+
+  let seeds = 0;
+  let peopleFed = 0;
+  let player = 'Zane';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="menu-ui">
+        <img className="fsf-logo" srcSet={fsfHeader} alt="Feeding South Florida logo" />
+        <br />
+        <div className="menu-icon">
+          <img className="hamburger-icon" srcSet={hamburgerIcon} alt="Hamburger icon" />
+          <p>MENU</p>
+        </div>
+        <br />
+          <div className="leaderboard">
+            <table>
+              <caption>LEADERBOARD</caption>
+              <hr />
+              <tr>
+                <th>No.</th>
+                <th>Username</th>
+                <th>Meals</th>
+              </tr>
+              <tr>
+                <td>#1</td>
+                <td>User</td>
+                <td>500</td>
+              </tr>
+              <tr>
+                <td>#2</td>
+                <td>User</td>
+                <td>187</td>
+              </tr>
+              <tr>
+                <td>#3</td>
+                <td>User</td>
+                <td>99</td>
+              </tr>
+              <tr>
+                <td>#4</td>
+                <td>User</td>
+                <td>67</td>
+              </tr>
+              <tr>
+                <td>#5</td>
+                <td>User</td>
+                <td>55</td>
+              </tr>
+              <tr>
+                <td>#6</td>
+                <td>User</td>
+                <td>32</td>
+              </tr>
+              <hr />
+              <thead>YOUR RANK</thead>
+              <tr>
+                <td>#100</td>
+                <td>You</td>
+                <td>0</td>
+              </tr>
+            </table>
+          </div>
+          <form action="https://volunteer.feedingsouthflorida.org/" target="_blank">
+            <input type="submit" value="VOLUNTEER NOW" />
+          </form>
+      </div>
+      <div class="vr"></div>
+      <div className="main-ui">
+        <h1>Farm to Feed Florida</h1>
+        <div className="game-ui">
+          <div className="seeds">
+            <div className="score">
+              <p>{seeds} SEEDS</p>
+            </div>
+            <p>START PLANTING!</p>
+          </div>
+          <iframe title="Farm to Feed Florida" width="600" height="450" />
+          <div className="people-fed">
+            <p className="score">YOU'VE PROVIDED MEALS FOR {peopleFed} SO FAR!</p>
+          </div>
+        </div>
+        {/*<div class="scores">
+          <p class="score">Player: {player}</p>
+        </div>*/}
+        <div className="sponsors">
+          <p><u>SPONSOR LEADERBOARD</u></p>
+          <div className="ads">
+            <svg fill="gray" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/><text x="5" y="16" fill="white">#1</text></svg>
+            <img className="ad" srcSet={officeDepot} alt="Advertising will be shown here."/>
+            
+            <svg fill="gray" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/><text x="5" y="16" fill="white">#2</text></svg>
+            <img className="ad" srcSet={bocaCode} alt="Advertising will be shown here."/>
+            
+            <svg fill="gray" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/><text x="5" y="16" fill="white">#3</text></svg>
+            <img className="ad" srcSet={aws} alt="Advertising will be shown here."/>
+            
+            <svg fill="gray" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/><text x="5" y="16" fill="white">#4</text></svg>
+            <img className="ad" srcSet={fpl} alt="Advertising will be shown here."/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
